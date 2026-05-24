bb271f00cedb40e491c466adaaf64831a6f2d798
edave64 <edave64@users.noreply.github.com>
2026-05-24 21:15:26 +0000

    refactor(ts): leftover require

--- ts-src/main.ts
+++ ts-src/main.ts
@@ -11,7 +11,6 @@ app.whenReady().then(() => {
 		// On macOS it's common to re-create a window in the app when the
 		// dock icon is clicked and there are no other windows open.
 		if (BrowserWindow.getAllWindows().length === 0) {
-			const window = require('./components/window.js');
 			window.open();
 		}
 	});
